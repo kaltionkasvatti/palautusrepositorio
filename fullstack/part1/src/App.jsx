3a31b3bf2d6e309372c1ff7a92ede518aa51465e
Hánno Länsman <halansman@gmail.com>
2026-03-20 15:16:13 +0200

    Part 1 a exercises

--- fullstack/part1/src/App.jsx
+++ fullstack/part1/src/App.jsx
@@ -1,18 +1,65 @@
-const Hello = () => {
+const Header = (props) => {
+  console.log(props)
+  return(
+  <>
+    <h1>
+      {props.course}
+    </h1>
+  </>
+  )
+}
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part} {props.exercise}
+      </p>
+    </>
+  )
+}
+
+
+const Content = (props) => {
+  console.log(props)
+  console.log(props.parts[0])
   return (
     <div>
-      <p>Hello world</p>
+      <Part part={props.parts[0]} exercise={props.exercises[0]}/>
+      <Part part={props.parts[1]} exercise={props.exercises[1]}/>
+      <Part part={props.parts[2]} exercise={props.exercises[2]}/>
     </div>
   )
 }
 
+const Total = (props) => {
+  const total = props.exercises.reduce((total, num) => total + num, 0)
+  console.log(total)
+
+  return (
+    <p>
+      Number of exercises {total}
+    </p>
+  )
+}
+
 const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    'Fundamentals of React',
+    'Using props to pass data',
+    'State of a component'
+  ]
+  const exercises = [
+    10,
+    7,
+    14
+  ]
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello /> 
+      <Header course={course} />
+      <Content parts={parts} exercises={exercises} />
+      <Total exercises={exercises} />
     </div>
   )
 }
